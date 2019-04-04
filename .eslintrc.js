52d114d008aca4c5634c0ff2f69bc04b7c97a457
txs1992 <xusheng.tao@ele.me>
2019-04-04 15:22:28 +0800

    feat：添加 格式化插件与 CI 配置。

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,18 @@
 module.exports = {
   root: true,
   parserOptions: {
-    parser: "babel-eslint",
+    parser: 'babel-eslint',
     ecmaVersion: 6,
-    sourceType: "module"
+    sourceType: 'module'
   },
   env: {
     browser: true
   },
-  extends: ["standard", "plugin:prettier/recommended", "prettier/standard"],
-  plugins: ["prettier"],
+  extends: ['standard', 'plugin:prettier/recommended', 'prettier/standard'],
+  plugins: ['prettier'],
   rules: {
-    "arrow-parens": 0,
-    "generator-star-spacing": 0,
-    "no-debugger": 0
+    'arrow-parens': 0,
+    'generator-star-spacing': 0,
+    'no-debugger': 0
   }
-};
+}
