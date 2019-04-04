1fd0fb9a3288600ec2450822ae4697ee3b1badee
txs1992 <xusheng.tao@ele.me>
2019-04-04 15:41:42 +0800

    fix：修改一次错误的配置。

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,12 @@
-import { terser } from "rollup-plugin-terser";
-import commonjs from "rollup-plugin-commonjs";
+import { terser } from 'rollup-plugin-terser'
+import commonjs from 'rollup-plugin-commonjs'
 
 export default {
-  input: "src/index.js",
+  input: 'src/index.js',
   output: {
-    file: "lib/index.js",
-    format: "umd",
-    name: "MtUtils"
+    file: 'lib/index.js',
+    format: 'umd',
+    name: 'MtUtils'
   },
   plugins: [commonjs(), terser()]
-};
+}
