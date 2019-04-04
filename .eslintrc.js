3c32cf3c72c6cf0c40e1502729e573eb16e483ed
txs1992 <xusheng.tao@ele.me>
2019-04-04 15:20:37 +0800

    feat：添加 eslint rollup 等相关配置。

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    parser: "babel-eslint",
+    ecmaVersion: 6,
+    sourceType: "module"
+  },
+  env: {
+    browser: true
+  },
+  extends: ["standard", "plugin:prettier/recommended", "prettier/standard"],
+  plugins: ["prettier"],
+  rules: {
+    "arrow-parens": 0,
+    "generator-star-spacing": 0,
+    "no-debugger": 0
+  }
+};
---
+++ rollup.config.js
@@ -0,0 +1,12 @@
+import { terser } from "rollup-plugin-terser";
+import commonjs from "rollup-plugin-commonjs";
+
+export default {
+  input: "src/index.js",
+  output: {
+    file: "lib/index.js",
+    format: "umd",
+    name: "MtUtils"
+  },
+  plugins: [commonjs(), terser()]
+};
