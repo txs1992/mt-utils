73e4c15abdcf67179f053a8c81aec520ac5bec88
txs1992 <xusheng.tao@ele.me>
2019-04-08 14:19:39 +0800

    feat：添加文档。

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import obj2qs from './obj2qs.js'
 import isObject from './is-object.js'
 import cssSupports from './css-supports.js'
+import getEndDateOfWeek from './get-end-date-of-week.js'
 import isScrollElement from './is-scroll-element.js'
 import getScrollElementOfFirstChild from './get-scroll-element-of-first-child.js'
 import getScrollElementOfFirstParent from './get-scroll-element-of-first-parent.js'
@@ -9,6 +10,7 @@ export default {
   obj2qs,
   isObject,
   cssSupports,
+  getEndDateOfWeek,
   isScrollElement,
   getScrollElementOfFirstChild,
   getScrollElementOfFirstParent
